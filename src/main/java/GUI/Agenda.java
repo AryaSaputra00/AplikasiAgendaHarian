9d164da4e39512e3049db3af1e8a5db403db2d08
AryaSaputra <abangputra097@gmail.com>
2023-11-26 19:05:08 +0800

    first commit

---
+++ src/main/java/GUI/Agenda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+/**
+ *
+ * @author ACER A5145433WF
+ */
+class Agenda {
+    
+}
